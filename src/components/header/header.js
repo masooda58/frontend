57341e31915637e42bca5bfe736c779fc4bf47e8
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-02-18 18:47:13 +0330

    »نو های نهای بدون دکمه ها

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,23 +29,23 @@ export default function Header({ menuToggleEnabled, title, toggleMenu }) {
         {/*<Item location={'before'}>*/}
         {/*  <TabNavigation/>*/}
         {/*</Item>*/}
-        <Item
-          location={'after'}
-          locateInMenu={'auto'}
-          menuItemTemplate={'userPanelTemplate'}
-        >
-          <Button
-            className={'user-button authorization'}
-            width={210}
-            height={'100%'}
-            stylingMode={'text'}
-          >
-            <UserPanel menuMode={'context'} />
-          </Button>
-        </Item>
-        <Template name={'userPanelTemplate'}>
-          <UserPanel menuMode={'list'} />
-        </Template>
+        {/*<Item*/}
+        {/*  location={'after'}*/}
+        {/*  locateInMenu={'auto'}*/}
+        {/*  menuItemTemplate={'userPanelTemplate'}*/}
+        {/*>*/}
+        {/*  <Button*/}
+        {/*    className={'user-button authorization'}*/}
+        {/*    width={210}*/}
+        {/*    height={'100%'}*/}
+        {/*    stylingMode={'text'}*/}
+        {/*  >*/}
+        {/*    <UserPanel menuMode={'context'} />*/}
+        {/*  </Button>*/}
+        {/*</Item>*/}
+        {/*<Template name={'userPanelTemplate'}>*/}
+        {/*  <UserPanel menuMode={'list'} />*/}
+        {/*</Template>*/}
       </Toolbar>
     </div>
 )}
